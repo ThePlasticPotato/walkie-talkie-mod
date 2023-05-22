14c8f432c41ff2142b8ad18c4ede33c37aec1f13
ThePlasticPotato <77798144+ThePlasticPotato@users.noreply.github.com>
2023-05-22 17:22:27 -0400

    forgor models and an if statement :skull:

--- src/main/java/com/potatomato/walkietalkie/network/packet/s2c/ButtonS2CPacket.java
+++ src/main/java/com/potatomato/walkietalkie/network/packet/s2c/ButtonS2CPacket.java
@@ -1,6 +1,8 @@
 package com.potatomato.walkietalkie.network.packet.s2c;
 
+import com.potatomato.walkietalkie.WalkieTalkie;
 import com.potatomato.walkietalkie.client.gui.screen.WalkieTalkieScreen;
+import com.potatomato.walkietalkie.item.WalkieTalkieItem;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
@@ -12,8 +14,8 @@ public class ButtonS2CPacket {
     public static void receive(MinecraftClient minecraftClient, ClientPlayNetworkHandler clientPlayNetworkHandler, PacketByteBuf packet, PacketSender packetSender) {
 
         ItemStack stack = packet.readItemStack();
-
-        WalkieTalkieScreen.getInstance().checkButtons(stack);
-
+        if (stack.getItem().getClass().equals(WalkieTalkieItem.class)) {
+            WalkieTalkieScreen.getInstance().checkButtons(stack);
+        }
     }
 }
